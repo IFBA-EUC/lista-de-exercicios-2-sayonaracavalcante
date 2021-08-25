b7b3119ee403e99fdf0eaa3d5516100989ab13d6
sayonaracavalcante <89099783+sayonaracavalcante@users.noreply.github.com>
2021-08-24 23:54:52 -0300

    Create questão9.java

---
+++ questão9.java
@@ -0,0 +1,15 @@
+public class questão9 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 8; i = i + 2) {
+            for (int j = i; j <= 4; j++) {
+                for (int k = 1; k <= j; k = k + i) {
+                    System.out.println(i + ", " + j + ", " + k);
+                }
+            }
+        }
+    }
+}
+
+
+// A saída do seguite programa será:
+//2, 2, 2
